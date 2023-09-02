6f13419ac32c321e37ca36440cf686fb97461be1
andrestolher <102180177+andrestolher@users.noreply.github.com>
2023-09-01 23:44:00 -0600

    FINAL COMMIT

--- src/main/java/com/andres/app/apirest/Controller/ToDoController.java
+++ src/main/java/com/andres/app/apirest/Controller/ToDoController.java
@@ -1,12 +1,41 @@
 package com.andres.app.apirest.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.andres.app.apirest.Model.Task;
+import com.andres.app.apirest.Repository.ToDoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ToDoController {
-    @GetMapping(value = "/")
-    public String holaMundo(){
-        return "Hola Mundo";
+    @Autowired
+    private ToDoRepository toDoRepository;
+
+    @GetMapping(value = "/tasks")
+    public List <Task> getTask(){
+        return toDoRepository.findAll();
+    }
+
+    @PostMapping(value = "/saveTask")
+    public String saveTask(@RequestBody Task task){
+        toDoRepository.save(task);
+        return "SavedTask";
+    }
+
+    @PutMapping(value = "/update/{id}")
+    public String updateTask(@PathVariable long id, @RequestBody Task task){
+        Task updateTask = toDoRepository.findById(id).get();
+        updateTask.setTitle(task.getTitle());
+        updateTask.setState(task.getState());
+        toDoRepository.save(updateTask);
+        return "UpdatedTask";
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public String deleteTask(@PathVariable long id){
+        Task deleteTask = toDoRepository.findById(id).get();
+        toDoRepository.delete(deleteTask);
+        return "DeletedTask";
     }
 }
